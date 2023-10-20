5217d55712d5f8a4f1e5313032fb34a97503ef33
pabsi <pablosierralorente@outlook.es>
2023-10-20 16:28:13 +0200

    unitary test Validator constructor

---
+++ tests/test_validator_constructor.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import MagicMock
+from pyquality.pyquality import Validator
+from pyquality.utilities.errors import SparkSessionError
+
+class TestConstructorPyQuality(unittest.TestCase):
+
+    def test_constructor_with_spark(self):
+        spark_mock = MagicMock()
+        validator_test = Validator(spark=spark_mock)
+        self.assertEqual(validator_test.spark, spark_mock)
+
+    def test_constructor_without_spark(self):
+        validator_test = Validator()
+        self.assertIsNotNone(validator_test.spark)
+
+    def test_close_with_spark(self):
+        spark_mock = MagicMock()
+        validator_test = Validator(spark=spark_mock)
+        validator_test.close()
+        spark_mock.stop.assert_called_once()
+    
+    def test_close_without_spark(self):
+        validator_test = Validator()
+        with self.assertRaises(SparkSessionError):
+            validator_test.close()
+
+if __name__ == '__main__':
+    unittest.main()
